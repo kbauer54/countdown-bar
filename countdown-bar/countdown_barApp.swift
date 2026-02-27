127b2e8f7c7443ea1313b32230c2629d618df530
Kolbe Bauer <kolbebauer@Kolbes-MacBook-Pro-2.local>
2026-02-27 10:45:26 -0600

    Initial commit

---
+++ countdown-bar/countdown_barApp.swift
@@ -0,0 +1,17 @@
+//
+//  countdown_barApp.swift
+//  countdown-bar
+//
+//  Created by Kolbe Bauer on 2/27/26.
+//
+
+import SwiftUI
+
+@main
+struct countdown_barApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
